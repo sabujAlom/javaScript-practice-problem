f9cefdadf2c71269262858c1ba2e4a53454bfad6
sabujAlom <sabujalom18@gmail.com>
2026-02-09 22:34:25 +0600

    problem03 completed

---
+++ problem03.js
@@ -0,0 +1,11 @@
+function operation(number){
+   if(number % 2 ===0){
+       return "this number is even";
+   }
+   else{
+     return "this number is odd"
+   }
+   
+}
+const output = operation(100);
+console.log(output);
