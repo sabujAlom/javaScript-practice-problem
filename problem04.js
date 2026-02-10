e0af756f43cc98140cd355c8952a10d392947a9a
sabujAlom <sabujalom18@gmail.com>
2026-02-10 11:50:41 +0600

    problem 04 completed

---
+++ problem04.js
@@ -0,0 +1,12 @@
+function check(number){
+    if(number > 0){
+        console.log(number +' is positive number')
+    }
+    else if(number < 0){
+        console.log(number + ' is negative number');
+    }
+    else if(number === 0){
+        console.log(number + ' is this zero')
+    }
+}
+check(0);
